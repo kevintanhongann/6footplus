99451dc3fc73a9312c45e3905cf3094c97ab1fdd
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-03-27 15:50:08 +0000

    First Grails version

---
+++ 6footplus/test/unit/com/sixfootplus/blog/BlogUserTests.groovy
@@ -0,0 +1,17 @@
+package com.sixfootplus.blog
+
+import grails.test.*
+
+class BlogUserTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
