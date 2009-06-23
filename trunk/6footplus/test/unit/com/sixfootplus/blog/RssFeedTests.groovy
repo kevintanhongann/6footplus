4e60d854af1c437833bcf9c0acea5c5e1aa59ece
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-06-23 16:19:20 +0000

    Wrote some proper unit tests for domain object RssFeed

--- trunk/6footplus/test/unit/com/sixfootplus/blog/RssFeedTests.groovy
+++ trunk/6footplus/test/unit/com/sixfootplus/blog/RssFeedTests.groovy
@@ -3,15 +3,43 @@ package com.sixfootplus.blog
 import grails.test.*
 
 class RssFeedTests extends GrailsUnitTestCase {
+
+    def feed
+
     protected void setUp() {
+
         super.setUp()
+
+        // Make sure we can invoke validate() on our domain object.
+        mockForConstraintsTests(RssFeed)
+
+        feed = new RssFeed()
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testConstraintsNullable() {
 
+        assertFalse feed.validate()
+
+        assertEquals 'nullable', feed.errors['producer']
+        assertEquals 'nullable', feed.errors['title']
+        assertEquals 'nullable', feed.errors['description']
+        assertEquals 'nullable', feed.errors['link']
+        assertEquals 'nullable', feed.errors['thumbnail']
+        assertEquals 'nullable', feed.errors['pubDate']
+
+        assertEquals 6, feed.errors.allErrors.size()
+    }
+
+    void testToString() {
+
+        def date = new Date()
+
+        feed = new RssFeed(producer: "a", title: "b", description: "c", link: "d", thumbnail: "e", pubDate: date)
+
+        assertEquals "a, b, c, d, e, " + date, feed.toString()
     }
 }
