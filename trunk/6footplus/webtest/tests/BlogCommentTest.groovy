e8a11b9cbbbe30067287a8ce448dc472f628fc70
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-06-25 21:56:17 +0000

    Code alignment changes only

--- trunk/6footplus/webtest/tests/BlogCommentTest.groovy
+++ trunk/6footplus/webtest/tests/BlogCommentTest.groovy
@@ -16,25 +16,25 @@ class BlogCommentTest extends AbstractWebTesting {
         test01Login()
 
         group(description:'List of comments') {
-            verifyText 'Sort by'
-            clickLink 'Next'
-            clickLink 'Previous'
+            verifyText  'Sort by'
+            clickLink   'Next'
+            clickLink   'Previous'
         }
 
         group(description:'Edit comment') {
             clickLink(label: 'edit', href: '/blogComment/edit/105');
-            verifyText 'Author'
-            verifyText 'IP'
-            verifyText 'Message'
-            verifyText 'Article'
-            verifyText 'Created'
+            verifyText  'Author'
+            verifyText  'IP'
+            verifyText  'Message'
+            verifyText  'Article'
+            verifyText  'Created'
             clickButton 'Update'
-            verifyText 'Comment 105 updated'
+            verifyText  'Comment 105 updated'
         }
 
         group(description:'Delete comment') {
             clickLink(label: 'delete', href: '/blogComment/delete/106');
-            verifyText 'BlogComment 106 deleted'
+            verifyText  'BlogComment 106 deleted'
         }
     }
 }
