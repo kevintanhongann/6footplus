934cc4e889be1edb703f018d947291b20ed6ce95
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-06-25 21:55:28 +0000

    Improved and extended canoo web tests

--- trunk/6footplus/webtest/tests/HomeTest.groovy
+++ trunk/6footplus/webtest/tests/HomeTest.groovy
@@ -1,60 +1,56 @@
-class HomeTest extends grails.util.WebTest {
+class HomeTest extends AbstractWebTesting {
 
-    def test01Index() {
-        
+    def test01Login() {
         invoke      '/'
         verifyText  'Beitrag Nr.1'
-        verifyText  'Dies ist ein Testeintrag mit der Nr.1'
-        verifyText  'comments (0)'
-        verifyText  'Previous'
-        verifyText  'my recent tweets'
-        verifyText  'my recent videos'
-        verifyText  'my recent photos'
-        verifyText  'my recent twitpics'
-        verifyText  'my contact details'
-        verifyText  'my recent articles'
-        verifyText  'more stuff'
     }
 
-    def test02Comments() {
+    def test02IndexCommentsPreviousAndShow() {
 
-        invoke      '/'
+        group(description:'Verify home page'){
+            invoke      '/'
+            verifyText  'Beitrag Nr.1'
+            verifyText  'Dies ist ein Testeintrag mit der Nr.1'
+            verifyText  'comments (0)'
+            verifyText  'Previous'
+            verifyText  'my recent tweets'
+            verifyText  'my recent videos'
+            verifyText  'my recent photos'
+            verifyText  'my recent twitpics'
+            verifyText  'my contact details'
+            verifyText  'my recent articles'
+            verifyText  'more stuff'
+        }
 
-        clickLink   'comments (0)'
+        group(description:'View and submit comment'){
+            invoke      '/'
+            clickLink   'comments (0)'
+            setInputField(name: 'author', value: 'A name')
+            setInputField(name: 'message', value: 'A comment')
+            clickButton "submit your comment"
+            //for some reason cannot read ajax response text, so have to reload page
+            invoke      '/'
+            verifyText  'comments (1)'
+        }
 
-        setInputField(name: 'author', value: 'A name')
-        setInputField(name: 'message', value: 'A comment')
+        group(description: 'Paginate list of articles'){
+            invoke      '/'
+            clickLink   'Previous'
+            verifyText  'Beitrag Nr.3'
+            clickLink   'Next'
+            verifyText  'Beitrag Nr.1'
+        }
 
-        clickButton "submit your comment"
-
-        //for some reason cannot read ajax response text, so have to reload page
-        invoke      '/'
-        verifyText  'comments (1)'
-    }
-
-    def test03Previous() {
-
-        invoke      '/'
-
-        clickLink   'Previous'
-        verifyText  'Beitrag Nr.2'
-    }
-
-    def test04Show() {
-
-        invoke      '/'
-
-        clickLink   'Beitrag Nr.1'
-        verifyText  'Beitrag Nr.1'
-
-        clickLink   'comments (1)'
-
-        setInputField(name: 'author', value: 'B name')
-        setInputField(name: 'message', value: 'B comment')
-
-        clickButton "submit your comment"
-
-        invoke      '/'
-        verifyText  'comments (2)'
+        group(description: 'Show article'){
+            invoke      '/'
+            clickLink   'Beitrag Nr.1'
+            verifyText  'Beitrag Nr.1'
+            clickLink   'comments (1)'
+            setInputField(name: 'author', value: 'B name')
+            setInputField(name: 'message', value: 'B comment')
+            clickButton "submit your comment"
+            invoke      '/'
+            verifyText  'comments (2)'
+        }
     }
 }
