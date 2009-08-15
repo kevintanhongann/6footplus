ec36ed937fcad95927f9569e7280394c38e9ab77
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-08-15 17:54:59 +0000

    Required for cobertura

---
+++ trunk/6footplus/grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,21 @@
+coverage {
+    exclusions = [
+        '**/com/burtbeckwith/**',
+        '**/com/studentsonly/**',
+        '**/feedsplugin/**',
+        '**/grails/util/**',
+        '**/org/grails/**',
+        '**/org/jsecurity/grails/**',
+        '**/winterwell/jtwitter/**',
+        '**/AuthController*',
+        '**/FormHelperTagLib*',
+        '**/BuildConfig*',
+        '**/FeedsTagLib*',
+        '**/JQueryTagLib*',
+        '**/JobManagerService*',
+        '**/Jsec*',
+        '**/Searchable*',
+        '**/SecurityFilters*',
+        '**/TwitterFilter*',
+        '**/*QuartzConfig*']
+}
