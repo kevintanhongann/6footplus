f4603aad7869f1da1ebf6f66c625133b70dcbb62
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2009-11-20 11:44:55 +0000

    Setting captcha value explicitly, but only in test environment (see webtest)

--- 6footplus/grails-app/taglib/com/sixfootplus/blog/CaptchaTagLib.groovy
+++ 6footplus/grails-app/taglib/com/sixfootplus/blog/CaptchaTagLib.groovy
@@ -1,14 +1,26 @@
 package com.sixfootplus.blog
 
+import grails.util.*
+
 class CaptchaTagLib {
 
     static namespace = "cm"
 
     def captchaRequest = { attrs ->
+
         def words = ['zero','one','two','three','four','five','six','seven','eight','nine','ten']
         def a = new Random().nextInt(10)
         def b = new Random().nextInt(10)
-        session.captcha = (a + b).toString()
-        out << "What is ${words[a]} + ${words[b]}?"
+
+        //hack by setting captcha to specific value for testing purposes
+        switch(GrailsUtil.environment) {
+            case "test":
+            session.captcha = "999"
+            out << "What is 1 + 998?"
+            break
+            default:
+            session.captcha = (a + b).toString()
+            out << "What is ${words[a]} + ${words[b]}?"
+        }
     }
 }
