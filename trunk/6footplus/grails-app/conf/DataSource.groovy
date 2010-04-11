f3d19332d5e8ced8d694c5e0ea4779af9b1f7158
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2010-04-11 10:15:25 +0000

    Changed caching to use EhCacheProvider

--- trunk/6footplus/grails-app/conf/DataSource.groovy
+++ trunk/6footplus/grails-app/conf/DataSource.groovy
@@ -4,33 +4,38 @@ dataSource {
 	username = "sa"
 	password = ""
 }
+
 hibernate {
     cache.use_second_level_cache=true
     cache.use_query_cache=true
-    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
 }
-// environment specific settings
+
 environments {
 	development {
 		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:mem:devDB"
+                    dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+    		    pooled = true
+                    driverClassName = "com.mysql.jdbc.Driver"
+                    username = "#"
+                    password = "#"
+                    url = "#"
 		}
 	}
 	test {
 		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
+                    dbCreate = "update"
+                    url = "jdbc:hsqldb:mem:testDb"
 		}
 	}
 	production {
 		dataSource {
-		    pooled = true
-			dbCreate = "update"
-			driverClassName = "com.mysql.jdbc.Driver"
-			username = "#"
-			password = "#"
-			url = "jdbc:mysql://localhost:3306/#"
+                    dbCreate = "update"
+    		    pooled = true
+                    driverClassName = "com.mysql.jdbc.Driver"
+                    username = "#"
+                    password = "#"
+                    url = "#"
 		}
 	}
 }
