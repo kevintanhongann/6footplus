c06abf3f26ec9c8c5bc52a3a35173b621f28781f
andreas.nerlich <andreas.nerlich@b9810ddf-884b-0410-aeb3-6b42106ff796>
2010-04-24 18:03:11 +0000

    Excluded hsql and mysql drivers from war

--- 6footplus/grails-app/conf/BuildConfig.groovy
+++ 6footplus/grails-app/conf/BuildConfig.groovy
@@ -19,3 +19,7 @@ coverage {
         '**/TwitterFilter*',
         '**/*QuartzConfig*']
 }
+grails.war.resources = { stagingDir ->
+    delete(file:"${stagingDir}/WEB-INF/lib/mysql-connector-java-5.1.7-bin.jar")
+    delete(file:"${stagingDir}/WEB-INF/lib/hsqldb-1.8.0.10.jar")
+}
